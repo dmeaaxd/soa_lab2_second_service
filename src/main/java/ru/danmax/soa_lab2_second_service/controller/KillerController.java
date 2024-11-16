ee0af00cc98af707300ce3a9d31870589c829d34
Maksim Ushakov <dmeaaxd@gmail.com>
2024-11-16 14:41:58 +0300

    update

--- src/main/java/ru/danmax/soa_lab2_second_service/controller/KillerController.java
+++ src/main/java/ru/danmax/soa_lab2_second_service/controller/KillerController.java
@@ -10,50 +10,42 @@ import ru.danmax.soa_lab2_second_service.service.KillerService;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/killer")
 public class KillerController {
-    private final String corsOrigin = "*";
-
     private final KillerService killerService;
 
     public KillerController(KillerService killerService) {
         this.killerService = killerService;
     }
 
-    @CrossOrigin(origins = corsOrigin)
     @GetMapping
     public List<Person> getAllKillers() {
         return killerService.getAllKillers();
     }
 
-    @CrossOrigin(origins = corsOrigin)
     @GetMapping("/{id}")
     public ResponseEntity<?> getKillerById(@PathVariable Integer id) {
         return killerService.getKillerById(id);
     }
 
-    @CrossOrigin(origins = corsOrigin)
     @PutMapping("/{id}")
     public ResponseEntity<?> updateKiller(@PathVariable Integer id, @RequestBody KillerDTO killerDTO) {
         return killerService.updateKiller(id, killerDTO);
     }
 
-    @CrossOrigin(origins = corsOrigin)
     @PostMapping
     public ResponseEntity<?> createKiller(@RequestBody KillerDTO killerDTO) {
         return killerService.createKiller(killerDTO);
     }
 
-    @CrossOrigin(origins = corsOrigin)
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteKiller(@PathVariable Integer id) {
         return killerService.deleteKiller(id);
     }
 
 
-    @CrossOrigin(origins = corsOrigin)
     @PostMapping("/teams/create/{teamId}/{teamName}/{teamSize}/{startCaveId}")
     public ResponseEntity<?> createKillerTeam(
             @PathVariable Integer teamId,
@@ -66,7 +58,6 @@ public class KillerController {
     }
 
 
-    @CrossOrigin(origins = corsOrigin)
     @PutMapping("/team/{teamId}/move-to-cave/{caveId}")
     public ResponseEntity<?> moveKillerTeamToCave(@PathVariable Integer teamId, @PathVariable Integer caveId) {
         return killerService.moveKillerTeamToCave(teamId, caveId);
